41df2219448b5f671acf5c1767722fc249bddae3
Pankaj RJ <pankajjogale3@gmail.com>
2023-03-03 11:26:33 +0530

    registeration data to db,setup db

---
+++ index.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import userRoutes from "./routes/users.js";
+import authRoutes from "./routes/auth.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import postRoutes from "./routes/posts.js";
+import cookieParser from "cookie-parser";
+
+const app = express();
+//middlewares(allow to send json obj)
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
+
+app.use("/api/auth", authRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+
+app.listen(8800, () => {
+  console.log("API WRKing on 8800 port");
+});
