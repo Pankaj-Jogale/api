c4dc2c9b0742031c277905910c2bdff7522b1d72
Pankaj RJ <pankajjogale3@gmail.com>
2023-03-03 16:23:06 +0530

    login-backend

--- index.js
+++ index.js
@@ -10,7 +10,15 @@ import cookieParser from "cookie-parser";
 const app = express();
 //middlewares(allow to send json obj)
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", true);
+  next();
+});
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
